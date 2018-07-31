86b53cbb5e6b76159f572016bc59513911d3c9a3
prasannacs <prasannacs.dev@gmail.com>
2018-07-31 12:03:12 -0400

    Create index.js

---
+++ LogSubscriber/index.js
@@ -0,0 +1,23 @@
+// Imports the Google Cloud client library
+const BoxSDK = require('box-node-sdk');
+const Request = require("request");
+
+exports.imageSubscriber = (event, callback) => {
+    const pubsubMessage = event.data;
+    console.log('Google Vision - Image subscriber');
+    var str = Buffer.from(pubsubMessage.data, 'base64').toString();
+    console.log(str);
+
+    var fileName = str.slice(0, str.indexOf('-Skills-'));
+    str = str.slice(fileName.length + 8, str.length);
+    var fileId = str.slice(0, str.indexOf('-Skills-'));
+    str = str.slice(fileId.length + 8, str.length);
+    var readToken = str.slice(0, str.indexOf('-Skills-'));
+    str = str.slice(readToken.length + 8, str.length);
+    var writeToken = str;
+
+    console.log('fileName -- ', fileName);
+    console.log('fileId -- ', fileId);
+    console.log('readToken -- ', readToken);
+    console.log('writeToken -- ', writeToken);
+}
