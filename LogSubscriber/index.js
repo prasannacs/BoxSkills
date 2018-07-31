61fbf932360b86251ad432bc2bf1f4dd61cd5e28
prasannacs <prasannacs.dev@gmail.com>
2018-07-31 15:57:20 -0400

    Update index.js

--- LogSubscriber/index.js
+++ LogSubscriber/index.js
@@ -3,6 +3,12 @@ const BoxSDK = require('box-node-sdk');
 const PubSub = require(`@google-cloud/pubsub`);
 
 exports.imageSubscriber = (event, callback) => {
+    
+    const pubsub = new PubSub();
+    const subscriptionName = 'log-subscriber';
+
+    
+    /*
     const pubsubMessage = event.data;
     console.log('Google Vision - Image subscriber');
     var str = Buffer.from(pubsubMessage.data, 'base64').toString();
@@ -20,4 +26,33 @@ exports.imageSubscriber = (event, callback) => {
     console.log('fileId -- ', fileId);
     console.log('readToken -- ', readToken);
     console.log('writeToken -- ', writeToken);
+    */
+
+    function subscribe() {
+
+        const timeout = 60;
+
+        // References an existing subscription
+        const subscription = pubsub.subscription(subscriptionName);
+
+        // Create an event handler to handle messages
+        let messageCount = 0;
+        const messageHandler = message => {
+            console.log(`Received message ${message.id}:`);
+            console.log(`\tData: ${message.data}`);
+            console.log(`\tAttributes: ${message.attributes}`);
+            messageCount += 1;
+
+            // "Ack" (acknowledge receipt of) the message
+            message.ack();
+        };
+
+        // Listen for new messages until timeout is hit
+        subscription.on(`message`, messageHandler);
+        setTimeout(() => {
+            subscription.removeListener('message', messageHandler);
+            console.log(`${messageCount} message(s) received.`);
+        }, timeout * 1000);
+    }
+
 }
