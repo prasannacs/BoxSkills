338f680212276f6e2798e3f28623e32d4f591068
prasannacs <prasannacs.dev@gmail.com>
2018-08-05 18:41:48 -0400

    Update index.js

--- LogSubscriber/index.js
+++ LogSubscriber/index.js
@@ -3,6 +3,7 @@ const BoxSDK = require('box-node-sdk');
 
 exports.logSubscriber = (event, callback) => {
     
+    console.log('event -- ',event);
    const pubsubMessage = event.data;
     console.log('Log subscriber');
     var str = Buffer.from(pubsubMessage.data, 'base64').toString();
