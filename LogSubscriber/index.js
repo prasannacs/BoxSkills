95c99920fe133d668e24828aa99d8aa74f2baab5
prasannacs <prasannacs.dev@gmail.com>
2018-08-05 19:18:41 -0400

    Update index.js

--- LogSubscriber/index.js
+++ LogSubscriber/index.js
@@ -1,6 +1,13 @@
 // Imports the Google Cloud client library
-const BoxSDK = require('box-node-sdk');
+const BigQuery = require('@google-cloud/bigquery');
 
+// Your Google Cloud Platform project ID
+const projectId = 'sixth-hawk-194719';
+
+// Creates a client
+const bigquery = new BigQuery({
+  projectId: projectId,
+});
 exports.logSubscriber = (event, callback) => {
     
     console.log('event -- ',event);
@@ -9,5 +16,39 @@ exports.logSubscriber = (event, callback) => {
     var str = Buffer.from(pubsubMessage.data, 'base64').toString();
     console.log(str);
     
+      const datasetId = "box_skills";
+ const tableId = "file";
+ var date = new Date();
+ date.setMonth(8)
+ const datetime = BigQuery.datetime({
+  year: date.getFullYear(),
+  month: date.getMonth(),
+  day: date.getDay(),
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  seconds: date.getSeconds()
+});
+
+       const rows = [{file_id: "Tom", file_name: "some file", read_token: "read token", write_token: "write_token", created: datetime, updated: datetime }];
+
+      // Inserts data into a table
+  bigquery
+    .dataset(datasetId)
+    .table(tableId)
+    .insert(rows)
+    .then(() => {
+      console.log(`Inserted ${rows.length} rows`);
+    })
+    .catch(err => {
+      if (err && err.name === 'PartialFailureError') {
+        if (err.errors && err.errors.length > 0) {
+          console.log('Insert errors:');
+          err.errors.forEach(err => console.error(err));
+        }
+      } else {
+        console.error('ERROR:', err);
+      }
+    });
+
     
 }
